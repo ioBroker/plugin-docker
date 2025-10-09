ea5109155511b17082dcdf72a35459b3bc9cbf20
GermanBluefox <dogafox@gmail.com>
2025-10-09 11:43:21 +0200

    Tuning on exports

--- tasks.js
+++ tasks.js
@@ -1,5 +1,14 @@
-const { copyFileSync } = require('node:fs');
+const { copyFileSync, readFileSync, writeFileSync } = require('node:fs');
 copyFileSync(`${__dirname}/src/types.d.ts`, `${__dirname}/build/esm/types.d.ts`);
 copyFileSync(`${__dirname}/src/types.d.ts`, `${__dirname}/build/cjs/types.d.ts`);
 copyFileSync(`${__dirname}/package.json`, `${__dirname}/build/esm/package.json`);
 copyFileSync(`${__dirname}/package.json`, `${__dirname}/build/cjs/package.json`);
+let text = readFileSync(`${__dirname}/build/cjs/index.js`, 'utf8');
+text = text.replace(
+    'exports.default = DockerPlugin;',
+    `module.exports = DockerPlugin;
+module.exports.DockerManagerOfOwnContainers = DockerManagerOfOwnContainers;
+module.exports.DockerManager = DockerManager;
+module.exports.default = DockerPlugin;`,
+);
+writeFileSync(`${__dirname}/build/cjs/index.js`, text, 'utf8');
